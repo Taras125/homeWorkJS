92b1cfe38fcea37c8624ba4a776c903f95f4a313
Taras <taras.labyak.grand.gym@gmail.com>
2020-03-25 10:54:59 +0200

    homeJS

---
+++ js.js
@@ -0,0 +1,8 @@
+const secondsInAMinute = 60;
+const minutesInAnHour = 60;
+const secondsInAnHour = secondsInAMinute * minutesInAnHour;
+const hoursInADay = 24;
+const secondsInADay = secondsInAnHour * hoursInADay;
+const daysInAYear = 365;
+const secondsInAYear = secondsInADay * daysInAYear;
+alert(secondsInAYear);
